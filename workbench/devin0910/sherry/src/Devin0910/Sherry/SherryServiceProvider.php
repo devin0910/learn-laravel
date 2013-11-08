100da4b957b4ee1126e29f0f109f8b3acd824d49
devin_yee <smartlyfly@gmail.com>
2013-11-08 16:41:28 +0800

    初始化项目

---
+++ workbench/devin0910/sherry/src/Devin0910/Sherry/SherryServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Devin0910\Sherry;
+
+use Illuminate\Support\ServiceProvider;
+
+class SherryServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('devin0910/sherry');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
